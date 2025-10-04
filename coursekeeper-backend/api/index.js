7c5f3c0b5b19b8bdcaf7f424c2cffee6a0f98f63
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-10-03 21:25:28 -0300

    :ambulance: chore: change index handler build path file

--- coursekeeper-backend/api/index.js
+++ coursekeeper-backend/api/index.js
@@ -4,7 +4,7 @@ const { parse } = require('url');
 // Import the compiled serverless handler from the build output
 let handler;
 try {
-  handler = require('../dist/src/serverless').default;
+  handler = require('../dist/serverless').default;
 } catch (e) {
   console.error(
     'Could not load serverless handler. Make sure to run `npm run build` before deploy.',
