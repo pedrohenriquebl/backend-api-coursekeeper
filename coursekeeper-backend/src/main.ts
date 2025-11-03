49b86087bb4594088a613bf216ceeeb0232d1bd7
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-11-03 12:37:35 -0300

    :construction: chore: update main ts to check db

--- coursekeeper-backend/src/main.ts
+++ coursekeeper-backend/src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -34,6 +34,26 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Cria Prisma Client
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+  try {
+    await prisma.$connect(); // conecta explicitamente
+    console.log('✅ Prisma connected successfully');
+  } catch (err) {
+    console.error('❌ Failed to connect to database:', err);
+    process.exit(1); // sai do processo se não conectar
+  }
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
