ba402b0cadd5ad68d6bdec08cf07d1c4377508e0
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-11-03 15:36:29 -0300

    :construction: feat: update cors and app service

--- coursekeeper-backend/src/app.service.ts
+++ coursekeeper-backend/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const frontendUrl =
+      process.env.FRONTEND_URL || 'https://frontend-coursekeeper.vercel.app/';
+    return `🚀 Backend is up and running! You can access the frontend here: ${frontendUrl}`;
   }
 }
--- coursekeeper-backend/src/main.ts
+++ coursekeeper-backend/src/main.ts
@@ -3,17 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaClient } from '@prisma/client';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://frontend-coursekeeper.vercel.app',
-      'http://localhost:3001',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  app.use(
+    cors({
+      origin: [
+        'https://frontend-coursekeeper.vercel.app',
+        'http://localhost:3001',
+      ],
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      allowedHeaders: '*',
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
